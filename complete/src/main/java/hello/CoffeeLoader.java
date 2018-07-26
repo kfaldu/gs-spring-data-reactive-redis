5817299b798305876751189b61df9c0153df3447
Krupesh <kfaldu@loyalty.com>
2018-07-26 11:13:20 -0400

    feat: create flux to load lots of data

--- complete/src/main/java/hello/CoffeeLoader.java
+++ complete/src/main/java/hello/CoffeeLoader.java
@@ -21,12 +21,27 @@ public class CoffeeLoader {
 
   @PostConstruct
   public void loadData() {
+
+    Flux<Coffee> coffeeFlux = Flux.create(fluxSink -> {
+      for (long i = 0; i < Math.pow(10, 7); ++i) {
+        fluxSink.next(new Coffee(UUID.randomUUID().toString(), "n:" + System.nanoTime()));
+      }
+    });
+
     factory.getReactiveConnection().serverCommands().flushAll().thenMany(
-        Flux.just("Jet Black Redis", "Darth Redis", "Black Alert Redis")
-            .map(name -> new Coffee(UUID.randomUUID().toString(), name))
+        coffeeFlux
             .flatMap(coffee -> coffeeOps.opsForValue().set(coffee.getId(), coffee)))
         .thenMany(coffeeOps.keys("*")
             .flatMap(coffeeOps.opsForValue()::get))
-        .subscribe(System.out::println);
+        .subscribe();
+
+//    factory.getReactiveConnection().serverCommands().flushAll().thenMany(
+//        Flux.just("Jet Black Redis", "Darth Redis", "Black Alert Redis")
+//            .map(name -> new Coffee(UUID.randomUUID().toString(), name))
+//            .flatMap(coffee -> coffeeOps.opsForValue().set(coffee.getId(), coffee)))
+//        .thenMany(coffeeOps.keys("*")
+//            .flatMap(coffeeOps.opsForValue()::get))
+//        .subscribe(System.out::println);
+
   }
 }
