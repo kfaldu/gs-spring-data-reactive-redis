62e7428871be9cc03ca4d7b553802a4fbe7e88af
Krupesh <kfaldu@loyalty.com>
2018-07-27 18:44:37 -0400

    refactor: use range instead of flux sink

--- complete/src/main/java/hello/CoffeeLoader.java
+++ complete/src/main/java/hello/CoffeeLoader.java
@@ -1,13 +1,18 @@
 package hello;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
 import javax.annotation.PostConstruct;
+import lombok.extern.java.Log;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.util.ByteUtils;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 @Component
+@Log
 public class CoffeeLoader {
 
   private final ReactiveRedisConnectionFactory factory;
@@ -19,21 +24,19 @@ public class CoffeeLoader {
     this.coffeeOps = coffeeOps;
   }
 
+  private static String toString(ByteBuffer byteBuffer) {
+    return new String(ByteUtils.getBytes(byteBuffer));
+  }
+
   @PostConstruct
   public void loadData() {
-
-    Flux<Coffee> coffeeFlux = Flux.create(fluxSink -> {
-      for (long i = 0; i < Math.pow(10, 7); ++i) {
-        fluxSink.next(new Coffee(UUID.randomUUID().toString(), "n:" + System.nanoTime()));
-      }
-    });
-
     factory.getReactiveConnection().serverCommands().flushAll().thenMany(
-        coffeeFlux
-            .flatMap(coffee -> coffeeOps.opsForValue().set(coffee.getId(), coffee)))
-        .thenMany(coffeeOps.keys("*")
-            .flatMap(coffeeOps.opsForValue()::get))
-        .subscribe();
+        Flux.range(0, (int) Math.pow(10, 3))
+            .map(key -> new Coffee(UUID.randomUUID().toString(), "n:" + System.nanoTime()))
+            .flatMap(coffee -> coffeeOps.opsForValue().set(coffee.getId(), coffee))
+    ).subscribe();
+
+    log.info(toString(factory.getReactiveConnection().keyCommands().randomKey().block()));
 
 //    factory.getReactiveConnection().serverCommands().flushAll().thenMany(
 //        Flux.just("Jet Black Redis", "Darth Redis", "Black Alert Redis")
